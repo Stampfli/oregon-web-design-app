10a67ec1ae10bbc2a50412834909ae10963a7456
Grant Stampfli <mail@gstampfli.com>
2015-02-18 13:53:28 -0800

    seting up proj w/ angularJS, Grunt, and Bootstrap.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('orWdApp').controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
